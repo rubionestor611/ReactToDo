bd9b1208bf3f52d256ee210b9dfc8cb2c845b8b5
Nestor Rubio <nestordrubio9@gmail.com>
2022-03-09 20:30:40 -0500

    some forms practice to later introduce to make the to-do list so much better

--- todo/src/Components/Form.js
+++ todo/src/Components/Form.js
@@ -6,18 +6,24 @@ class Form extends React.Component {
     super();
     this.state = {
       firstName: "",
-      lastName: ""
+      lastName: "",
+      isFriendly: true,
+      gender: "",
+      favColor: "blue"
     };
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event){
-    const {name, value} = event.target
-    this.setState({
-      [name]: value
-    });
+    const {name, value,type, checked} = event.target
+    if(type === "checkbox"){
+      this.setState({[name]: checked})
+    }else{
+      this.setState({ [name]: value });
+    }
   }
   render(){
     return(
+      // onSubmit can be used to handle the submission of forms
       <form className = "wrapper">
         <h1>Form Practice</h1>
         <input type = "text"
@@ -33,6 +39,60 @@ class Form extends React.Component {
           onChange={this.handleChange}/>
         <hr />
         <h1>{this.state.firstName} {this.state.lastName}</h1>
+        <hr />
+        <textarea value ={"default textarea text"} onChange={this.handleChange}/>
+        <hr />
+        <label>
+          <input
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          /> Is Friendly?
+        </label>
+        <hr />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="Male"
+            checked={this.state.gender === "Male"}
+            onChange={this.handleChange}
+          /> Male
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value= "Female"
+            checked={this.state.gender === "Female"}
+            onChange={this.handleChange}
+          /> Female
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="Non-Binary"
+            checked={this.state.gender === "Non-Binary"}
+            onChange={this.handleChange}
+          /> Non-Binary
+        </label>
+        <h1>You chose {this.state.gender}</h1>
+        <hr />
+        <label>Favorite Color: </label>
+        <select
+        value={this.state.favColor}
+        onChange={this.handleChange}
+        name="favColor">
+          <option value ="blue">blue</option>
+          <option value ="green">green</option>
+          <option value ="black">black</option>
+          <option value ="pink">pink</option>
+          <option value ="red">red</option>
+        </select>
+        <p>You chose {this.state.favColor}</p>
+        <button>Submit</button>
       </form>
     )
   }
